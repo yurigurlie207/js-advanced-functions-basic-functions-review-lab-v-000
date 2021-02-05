d9eb9ab2b2ebb0ec801891c23a80b2e3bc321a9d
Adrienne Yao <adrienne.yao@icloud.com>
2021-02-05 02:56:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,34 @@ function saturdayFun(activity="roller-skate") {
 let mondayWork = function(activity="go to the office") {
   return `This Monday, I will ${activity}.`
 }
+
+let wrapAdjective = function(style="*") {
+  return function(adjective="special") {
+    return `You are ${style}${adjective}${style}!`
+  }
+}
+
+const Calculator = {
+  add: function(a,b) {
+    return a + b;
+  },
+  subtract: function(a,b) {
+    return a - b
+  },
+  multiply: function(a,b) {
+    return a * b
+  },
+  divide: function(a,b) {
+    return a / b;
+  }
+}
+
+let actionApplyer = function(start, ray) {
+  let a = start
+
+  for (let i = 0; i < ray.length; i++ ){
+    a = ray[i](a)
+  }
+
+  return a
+}
